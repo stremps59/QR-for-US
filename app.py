f569bbd57f11598f102e38e3260cd3825e56e802
stremps59 <stremps@sbcglobal.net>
2025-05-28 15:12:12 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -50,4 +50,6 @@ def generate_qr():
     return jsonify({'message': 'QR code sent successfully'}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
